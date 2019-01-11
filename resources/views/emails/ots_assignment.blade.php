15732f38303e29aee2282ed5d8c599783aea1aef
Aaron <aaron.albertson@gmail.com>
2019-01-11 01:55:46 -0500

    Update ots_assignment.blade.php
    ZTL to ZDC

--- resources/views/emails/ots_assignment.blade.php
+++ resources/views/emails/ots_assignment.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <p>Dear {{ $ins->fullname }},</p>
-<p>You have been assigned an OTS. The details are listed below. Please reply to this email, or email the controller separately in order to schedule the OTS exam. If you have any questions, please contact the TA at <a href="mailto:ta@ztlartcc.org">ta@ztlartcc.org</a>.</p>
+<p>You have been assigned an OTS. The details are listed below. Please reply to this email, or email the controller separately in order to schedule the OTS exam. If you have any questions, please contact the TA at <a href="mailto:ta@vzdc.org">ta@vzdc.org</a>.</p>
 <br>
 <p><b>Controller Name:</b> {{ $ots->controller_name }}</p>
 <p><b>Position:</b> {{ $ots->position_name }}</p>
@@ -12,5 +12,5 @@
 <p>You can view more details from the OTS Center on the website. Please attempt to schedule the OTS in a timely manner.</p>
 <br>
 <p>Sincerely,</p>
-<p>vZTL Training Administrator</p>
+<p>vZDC Training Administrator</p>
 @endsection
