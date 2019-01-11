9d8e5405bcd03be6c8fa6bce56b10e3df21c7af2
Aaron <aaron.albertson@gmail.com>
2019-01-11 01:54:53 -0500

    Update new_feedback.blade.php
    ZTL to ZDC

--- resources/views/emails/new_feedback.blade.php
+++ resources/views/emails/new_feedback.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <p>Dear {{ $controller->full_name }},</p>
 
-<p>You have new feedback that has been approved. You can see the details below or from the <a href="https://www.ztlartcc.org/dashboard/controllers/profile">"My Profile"</a> page on the website.</p>
+<p>You have new feedback that has been approved. You can see the details below or from the <a href="https://www.vzdc.org/dashboard/controllers/profile">"My Profile"</a> page on the website.</p>
 <br>
 
 <p><b>Submitted at:</b> {{ $feedback->created_at }}</p>
@@ -19,9 +19,9 @@
 @endif
 <br>
 
-<p>If you have any thoughts/comments, please email the DATM at <a href="mailto:datm@ztlartcc.org">datm@ztlartcc.org</a>.</p>
+<p>If you have any thoughts/comments, please email the DATM at <a href="mailto:datm@vzdc.org">datm@vzdc.org</a>.</p>
 <br>
 
 <p>Sincerely,</p>
-<p>ZTL ARTCC Staff</p>
+<p>ZDC ARTCC Staff</p>
 @endsection
