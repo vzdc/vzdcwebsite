19a91db90ef6fc57dd6b34526eaf1500ac89ab68
Aaron <aaron.albertson@gmail.com>
2019-01-11 00:07:34 -0500

    Update email.blade.php

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -8,6 +8,6 @@
         ----------
         @yield('footer')
         <p><i>For entertainment purposes only. Do not use for real world purposes. Part of the VATSIM Network.</i></p>
-        <p><i>If you believe that you have received this email in error, please contact the vZTL ARTCC Webmaster at <a href="mailto:wm@ztlartcc.org">wm@ztlartcc.org</a>.</i></p>
+        <p><i>If you believe that you have received this email in error, please contact the vZDC ARTCC Webmaster at <a href="mailto:wm@vzdc.org">wm@vzdc.org</a>.</i></p>
     </body>
 </html>
