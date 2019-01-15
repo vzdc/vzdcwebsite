354475232043b5368fc26056560937cbd37b67d2
Ian Cowan <38896380+iccowan@users.noreply.github.com>
2019-01-15 12:24:30 -0500

    Added expiration date attribute

--- app/SoloCert.php
+++ app/SoloCert.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class SoloCert extends Model
 {
     protected $table = 'solo_certs';
     protected $fillable = ['id', 'controller_id', 'position', 'value', 'created_at', 'updated_at'];
+    
+    public function getExpirationDateAttribute() {
+        $date = Carbon::create($this->created_at)->addMonth;
+        $display = $date->format('m/d/Y');
+        
+        return $display;
+    }
 }
