4ce94835cb5105040647b9395ba06c3805fd74b3
Aaron <aaron.albertson@gmail.com>
2019-01-21 23:10:53 -0500

    Update accept.blade.php
    Adjusted visitor email message for ZDC

--- resources/views/emails/visit/accept.blade.php
+++ resources/views/emails/visit/accept.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <p>Dear {!! $visitor->name !!},</p>
 
-    <p>Congratulations, you have been accepted as a visitor in Washington.</p><br>
-    <p>You can find all the information for the various facilities within the ARTCC under the files page on the website, <a href="http://www.vzdc.org">www.vzdc.org</a>.</p><br>
+    <p>Congratulations! You have been accepted as a visitor in Washington; pending completion of your GRP competency check.</p><br>
+    <p>You can find all the information for the various facilities within the ARTCC website under the files page, <a href="http://www.vzdc.org/%22%3Ewww.vzdc.org</a>.</p><br>
     <p>Teamspeak 3 is used for all controller communications.</p><br>
-    <p>All visitors are certified to control minor fields through their current rating. It is highly recommended that you review the SOPs and LOAs located on the website prior to logging onto the network. Although it is not required, it is recommended to schedule a session with a mentor or instructor to go over the procedures in Dulles.</p><br>
-    <p>Once again, congratulations on being accepted as a visitor in Washington ARTCC and we hope to see you on the network soon! If you have any questions, feel free to email the DATM at <a href="mailto:datm@ztlartcc.org">datm@ztlartcc.org</a>.</p><br>
+    <p>All visitors are required to complete a GRP competency check within 30 days of joining. This will be completed by a combined GND/TWR session at KIAD - Washington-Dulles airport. Per VATSIM and VATUSA visiting controller policies if you fail to show competency up to and including your current rating your visiting status at ZDC will be revoked and you will be referred to your home facility for remedial training. Upon completion of the GRP competency check you will be certified for minor fields up to and including your current rating. You may begin training for major airport certification at this time.</p><br>
+    <p>Once again, congratulations on being accepted as a visitor in Washington ARTCC and we hope to see you on the network soon! If you have any questions, feel free to email the DATM at <a href="mailto:datm@vzdc.org">datm@vzdc.org</a>.</p><br>
 
     <p>Best regards,</p>
     <p>ZDC Visiting Staff</p>
