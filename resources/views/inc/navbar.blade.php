59a58effdf8915d633d68801a636d930a67c7af4
Aaron <aaron.albertson@gmail.com>
2019-03-27 10:08:23 -0400

    Update navbar.blade.php
    Removed auto-overflow

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,7 +16,7 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="pilots" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pilots</a>
-                    <div class="dropdown-menu" aria-labelledby="pilots"style="overflow: auto">
+                    <div class="dropdown-menu" aria-labelledby="pilots">
                         <a class="dropdown-item" href="http://www.aircharts.org/" target="_blank">Charts</a>
                         <a class="dropdown-item" href="http://www.vatstar.com/" target="_blank">VATSTAR Training</a>
                         <a class="dropdown-item" href="https://www.aviationweather.gov/" target="_blank">Weather</a>
