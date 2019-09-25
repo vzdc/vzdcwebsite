59b7f091f0ac0836752c16a3a0e4aa8075337305
Aaron <aaron.albertson@gmail.com>
2019-09-24 22:26:21 -0400

    Update new_ticket.blade.php

--- resources/views/dashboard/training/new_ticket.blade.php
+++ resources/views/dashboard/training/new_ticket.blade.php
@@ -11,12 +11,8 @@ New Training Ticket
     &nbsp;
 </div>
 <br>
-        <div class="col-sm-3">
-            <div class="card card-body" style="background-color:lightgrey">
                 <center><h5>Zulu/UTC Time Now:</h5></center>
                 <center><iframe style="pointer-events: none" src="https://freesecure.timeanddate.com/clock/i6hnccu7/fs16/tct/pct/bas6/bat6/bac777/pa8/tt0/tm2/th1/ta1/tb4" frameborder="0" width="200" height="64" allowTransparency="true"></iframe></center>
-            </div>
-        </div>
 <div class="container">
     {!! Form::open(['action' => 'TrainingDash@saveNewTicket']) !!}
         @csrf
