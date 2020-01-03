08089b42dc431f6de1649ffea6ae99b12dab360b
itsbenny99 <acpilot70@gmail.com>
2020-01-03 23:39:05 +0000

    #37

--- app/SoloCert.php
+++ app/SoloCert.php
@@ -2,18 +2,23 @@
 
 namespace App;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class SoloCert extends Model
 {
     protected $table = 'solo_certs';
-    protected $fillable = ['id', 'controller_id', 'position', 'value', 'created_at', 'updated_at'];
 
-    public function getExpirationDateAttribute() {
-        $date = $this->created_at->addMonth();
-        $display = $date->format('m/d/Y');
+    public function getPosTxtAttribute() {
+        if($this->pos == 0) {
+            $pos = 'Tower';
+        } elseif($this->pos == 1) {
+            $pos = 'Approach';
+        } elseif($this->pos == 2) {
+            $pos = 'Center';
+        } else {
+            $pos = 'N/A';
+        }
 
-        return $display;
+        return $pos;
     }
 }
