dad2fc037f6a6f0faf06e55b44933c2ed8e7156e
Aaron <aaron.albertson@gmail.com>
2020-01-05 17:38:47 -0500

    Update feedback.blade.php
    Update Google REcaptcha key

--- resources/views/site/feedback.blade.php
+++ resources/views/site/feedback.blade.php
@@ -64,7 +64,7 @@ New Feedback
             {!! Form::label('comments', 'Additional Comments:', ['class' => 'control-label']) !!}
             {!! Form::textArea('comments', null, ['placeholder' => 'Additional Comments', 'class' => 'form-control']) !!}
         </div>
-        <div class="g-recaptcha" data-sitekey="6LdZWMwUAAAAAAbX5WVKy9UG3Yf2swXPsqIKO4cf"></div>
+        <div class="g-recaptcha" data-sitekey="6Lf8lcwUAAAAAMVLQAZKTmZDKTknryEyZVhijmjz"></div>
         <br>
         <button class="btn btn-success" type="submit">Send Feedback</button>
     {!! Form::close() !!}
