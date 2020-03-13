53945bcbbad41afee687f8c90c4de4ed60ac614e
Aaron <aaron.albertson@gmail.com>
2020-03-13 10:27:18 -0400

    Create privacy_policy.blade.php
    Added privacy policy to site

---
+++ resources/views/site/privacy_policy.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.master')
+
+@section('title')
+vZDC - Privacy Policy
+@endsection
+
+@section('content')
+<span class="border border-light" style="background-color:#F0F0F0">
+    <div class="container">
+        &nbsp;
+        <h2>Privacy Policy</h2>
+	<br>
+	<br>
+vZDC ARTCC adheres and refers to <a href="https://www.vatusa.net/info/privacy">VATUSA</a> and <a href="https://www.vatsim.net/documents">VATSIM</a> on privacy policy matters.
+        &nbsp;
+    </div>
+</span>
+<br>
+@endsection
