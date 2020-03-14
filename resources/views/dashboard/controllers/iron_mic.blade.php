3fd7b96e9b180077e9404d63a857a82edd84abbf
Aaron <aaron.albertson@gmail.com>
2020-03-14 09:48:01 -0400

    Update iron_mic.blade.php

--- resources/views/dashboard/controllers/iron_mic.blade.php
+++ resources/views/dashboard/controllers/iron_mic.blade.php
@@ -5,5 +5,5 @@ vZDC - Iron Mic Standings
 @endsection
 
 @section('content')
-<iframe src="https://docs.google.com/spreadsheets/d/1LUrKtZFZiq50cdzlfOhDVezfy14BmtW1ALOKZ7kcmNM/pubhtml?widget=true&amp;headers=true" width="100%" height="1200"></iframe>
+<iframe src="https://docs.google.com/spreadsheets/d/1LUrKtZFZiq50cdzlfOhDVezfy14BmtW1ALOKZ7kcmNM/pubhtml?widget=true&amp;headers=true" width="100%" height="800"></iframe>
 @endsection
