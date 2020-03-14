742d6069fb80ca1403d26f67486dd26b0bc8ba75
Aaron <aaron.albertson@gmail.com>
2020-03-14 09:10:35 -0400

    Create currency_hours.blade.php
    initial commit

---
+++ resources/views/dashboard/controllers/currency_hours.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+vZDC - Currency Hours
+@endsection
+
+@section('content')
+<span class="border border-light" style="background-color:#F0F0F0">
+    <div class="container">
+        &nbsp;
+        <h2>Currency Hours</h2>
+	<br>
+    </div>
+</span>
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqTREEo7fnMvxhy0IpFwpZg0JVoFNM4hcsv-Th6zL4nr75ucC1mx_L_tm7t2tbBVXQY3g93CQLJ4t2/pubhtml?widget=true&amp;headers=false"></iframe>
+  
+  
+  
+  
+@endsection
