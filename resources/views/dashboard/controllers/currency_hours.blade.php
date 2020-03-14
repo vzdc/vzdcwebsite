2c3247da5c2c23c048a927ed2715e0de14e9f886
Aaron <aaron.albertson@gmail.com>
2020-03-14 09:29:32 -0400

    Update currency_hours.blade.php

--- resources/views/dashboard/controllers/currency_hours.blade.php
+++ resources/views/dashboard/controllers/currency_hours.blade.php
@@ -12,6 +12,7 @@ vZDC - Currency Hours
 	<br>
     </div>
 </span>
-<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqTREEo7fnMvxhy0IpFwpZg0JVoFNM4hcsv-Th6zL4nr75ucC1mx_L_tm7t2tbBVXQY3g93CQLJ4t2/pubhtml?widget=true&amp;headers=true" width="100%" height="1000"></iframe>
+<br>
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqTREEo7fnMvxhy0IpFwpZg0JVoFNM4hcsv-Th6zL4nr75ucC1mx_L_tm7t2tbBVXQY3g93CQLJ4t2/pubhtml?widget=true&amp;headers=true" width="100%" height="100%"></iframe>
 @endsection
 
