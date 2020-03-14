35d18e4fbd154b386355552b04da96103cbc3b5d
Aaron <aaron.albertson@gmail.com>
2020-03-14 09:34:29 -0400

    Create iron_mic.blade.php
    Initial Commit

---
+++ resources/views/dashboard/controllers/iron_mic.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+
+@section('title')
+vZDC - Iron Mic Standings
+@endsection
+
+@section('content')
+<iframe src="https://docs.google.com/spreadsheets/d/1LUrKtZFZiq50cdzlfOhDVezfy14BmtW1ALOKZ7kcmNM/pubhtml?widget=true&amp;headers=true" width="100%" height="100%"></iframe>
+@endsection
