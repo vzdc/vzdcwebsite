2920810e41c398a2b7602af6a2bfbb1483587d1b
Jacob Boyles <fixterjake@gmail.com>
2020-03-27 23:36:39 -0400

    Attempting to fix no recipient error attempt 3

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -1701,8 +1701,8 @@ class AdminDash extends Controller
             $submitter = auth()->user()->$id;
             $content = request()->get('content');
 
-            Mail::send(['html' => 'emails.member_log'], ['controller' => $controller, 'submitter' => $submitter, 'content' => $content], function ($m) {
-                $m->from('wm@vzdc.org', 'vZDC Website Logging');
+            Mail::send(['html' => 'emails.member_log'], ['controller' => $controller, 'submitter' => $submitter, 'content' => $content], function ($m) use ($controller) {
+                $m->from('wm@vzdc.org');
                 $m->subject('New Dossier Log Entry');
                 $m->to('atm@vzdc.org', 'datm@vzdc.org', 'ta@vzdc.org');
             });
