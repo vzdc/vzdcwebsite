915283febf570d962f72d366bb295ef33056c243
Jacob Boyles <fixterjake@gmail.com>
2020-03-29 02:29:12 -0400

    Added '-'s to the facility list and added check for dc center

--- app/Console/Commands/OnlineControllerUpdate.php
+++ app/Console/Commands/OnlineControllerUpdate.php
@@ -31,13 +31,13 @@ class OnlineControllerUpdate extends Command
 
     protected $facilities = [
 		/* BRAVO */
-		'DCA','IAD','BWI','PCT','ADW','DC_',
+		'DCA_','IAD_','BWI_','PCT_','ADW_','DC_',
 		/* CHARLIE */
-		'RIC','ROA','ORF','ACY','NGU','NTU','NHK','RDU',
+		'RIC_','ROA_','ORF_','ACY_','NGU_','NTU_','NHK_','RDU_',
 		/* DELTA */
-		'CHO','HGR','LYH','EWN','LWB','ISO','MTN','HEF','MRB','PHF','SBY','NUI','FAY','ILM','NKT','NCA','NYG','DAA','DOV','POB','GSB','WAL','CVN',
+		'CHO_','HGR_','LYH_','EWN_','LWB_','ISO_','MTN_','HEF_','MRB_','PHF_','SBY_','NUI_','FAY_','ILM_','NKT_','NCA_','NYG_','DAA_','DOV_','POB_','GSB_','WAL_','CVN_',
 		/* OBSERVER */
-		'ZDC'
+		'ZDC_'
 	];
 
     /**
@@ -95,7 +95,12 @@ class OnlineControllerUpdate extends Command
 
 				foreach ($this->facilities as $facility)
 				{
-					$is_controller = substr($position, 0, 3) == $facility;
+					if ($facility == 'dc_') {
+						$is_controller = substr($position, 0, 3) == $facility;
+					}
+					else {
+						$is_controller = substr($position, 0, 4) == $facility;
+					}
 					if ($is_controller) break;
 				}
 
