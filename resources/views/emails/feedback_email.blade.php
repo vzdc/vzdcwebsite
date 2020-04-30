eed0739a10dacd2d0caa9dfbb4147ec5e705b08b
Aaron <aaron.albertson@gmail.com>
2020-04-30 17:14:21 -0400

    Update feedback_email.blade.php

--- resources/views/emails/feedback_email.blade.php
+++ resources/views/emails/feedback_email.blade.php
@@ -6,4 +6,5 @@
 
 @section('footer')
     <p><b>Automated from vZDC ARTCC issued on the behalf of {{ $sender->full_name }}.</b></p>
+    <b>Please note this is a <u>NON-MONITORED</u> email box, if you have additional concerns / questions please email TA@vzdc.org.</b>
 @endsection
