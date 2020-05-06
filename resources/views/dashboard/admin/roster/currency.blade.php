5e2648ed12f8e77fa09013318d0c58e66c3bd79b
Jacob Boyles <fixterjake@gmail.com>
2020-05-06 04:43:48 -0400

    Changed to container-fluid for real this time

--- resources/views/dashboard/admin/roster/currency.blade.php
+++ resources/views/dashboard/admin/roster/currency.blade.php
@@ -20,33 +20,31 @@ Currency Manager
             </div>
         </div>
     </div>
-</div>
-<div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="home">
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Rating</th>
-                    <th scope="col">Total This Month</th>
-                    <th scope="col">Join Date</th>
-                    <th scope="col">Last training</th>
-                </tr>
-            </thead>
-            <tbody>
-                @for($i = 0; $i < count($controllers); $i++)
-                    @if($stats[$controllers[$i]->id]->total_hrs < 2)
-                        <tr>
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="home">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Rating</th>
+                        <th scope="col">Total This Month</th>
+                        <th scope="col">Join Date</th>
+                        <th scope="col">Last training</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for($i = 0; $i < count($controllers); $i++) @if($stats[$controllers[$i]->id]->total_hrs < 2) <tr>
                             <td>{{ $controllers[$i]->full_name }}</td>
                             <td>{{ $controllers[$i]->rating_short }}</td>
                             <td>{{ $stats[$controllers[$i]->id]->total_hrs }}</td>
                             <td>{{ $controllers[$i]->added_to_facility }}</td>
                             <td>{{ $controllers[$i]->getLastTrainingAttribute() }}</td>
-                        </tr>
-                    @endif
-                @endfor
-            </tbody>
-        </table>
+                            </tr>
+                            @endif
+                            @endfor
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
