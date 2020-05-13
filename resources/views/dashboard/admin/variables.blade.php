de546df18972a79ea9a05357e80644852ef8dcd0
Jacob Boyles <fixterjake@gmail.com>
2020-05-12 21:13:56 -0400

    Updates to styling

--- resources/views/dashboard/admin/variables.blade.php
+++ resources/views/dashboard/admin/variables.blade.php
@@ -6,22 +6,25 @@
 
 @section('content')
 <div class="container text-center">
+    <hr />
     <form action="/dashboard/admin/updatevisitorsvariable" method="POST">
         @csrf
         <input type="checkbox" class="form-check-input align-right" id="visitors" name="visitors">
         <label class="form-check-label" for="visitors">Allow Visitors</label>
+        <br />
         <button class="btn btn-primary" type="submit">Save Visitor Variable</button>
     </form>
     
     <br />
-    <hr />
     <br />
     
     <form action="/dashboard/admin/updatecurrencyvariable" method="POST">
         @csrf
         <input type="text" class="form-check-input align-right" id="currency" name="currency">
         <label class="form-check-label" for="currency">Set Currency Hours</label>
+        <br />
         <button class="btn btn-primary" type="submit">Save Currency Variable</button>
     </form>
+    <hr />
 </div>
 @endsection
