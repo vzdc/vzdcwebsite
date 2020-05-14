8b4524b3b2130513a5dc8feaecf51bfb736080f3
Jacob Boyles <fixterjake@gmail.com>
2020-05-14 02:49:19 -0400

    Trying to get vatusa authentication working

--- app/Http/Controllers/TrainingDash.php
+++ app/Http/Controllers/TrainingDash.php
@@ -404,11 +404,14 @@ class TrainingDash extends Controller
         $exam = ExamRequest::where('id', $id)->first();
 
         $client = new \GuzzleHttp\Client(['base_url' => 'https://api.vatusa.net/v2/exam/', 'timeout'  => 2.0,]);
-        $res = $client->request('POST', $exam->exam_id . "/assign/" . $exam->student_cid);
+        $res = $client->request('POST', $exam->exam_id . `/assign/` . $exam->student_cid, [
+            'auth' => ['']
+        ]);
 
         if ($res->getStatusCode() != 200) {
             return redirect()->back()->with('error', "Exam request assignment error, code: " . $res->getStatusCode());
         }
+        
         $exam->accepted = 0;
         $exam->assigned = 1;
         $exam->save();
@@ -419,7 +422,9 @@ class TrainingDash extends Controller
         $exam = ExamRequest::where('id', $id)->first();
 
         $client = new \GuzzleHttp\Client(['base_url' => 'https://api.vatusa.net/v2/exam/', 'timeout'  => 2.0,]);
-        $res = $client->request('POST', $exam->exam_id . "/assign/" . $exam->student_cid);
+        $res = $client->request('POST', $exam->exam_id . `/assign/` . $exam->student_cid, [
+            'auth' => ['']
+        ]);
 
         if ($res->getStatusCode() != 200) {
             return redirect()->back()->with('error', "Exam request assignment error, code: " . $res->getStatusCode());
