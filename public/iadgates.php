8c4ad47acb775988c9e4a0a46e49577b8fb0e050
Aaron <aaron.albertson@gmail.com>
2020-06-18 22:27:12 -0400

    Update iadgates.php
    Changed key to match aircraft colors.

--- public/iadgates.php
+++ public/iadgates.php
@@ -504,11 +504,11 @@
     <div id="map" style="width: 1200px; height: 600px"></div>
     <br>
     <table border=1>
-      <tr><td><font color ="green">No Flight Plan:</font></td><td style="background: #A37547;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
+      <tr><td><font color ="green">No Flight Plan:</font></td><td style="background: #FF00FF;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
       <!-- <td>Departures:</td><td style="background: #27c106;">&nbsp;&nbsp;&nbsp;&nbsp;</td> -->
       <tr><td><font color ="green">&nbsp;&nbsp;Parked:</font></td><td style="background: #ff0000;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
-      <tr><td><font color ="green">&nbsp;&nbsp;Taxiing (Dep):</font></td><td style="background: #FFFF00;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
-      <tr><td><font color ="green">&nbsp;&nbsp;Taxiing (Arr):</font></td><td style="background: #002af0;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
+      <tr><td><font color ="green">&nbsp;&nbsp;Taxiing (Dep):</font></td><td style="background: #00FF00;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
+      <tr><td><font color ="green">&nbsp;&nbsp;Taxiing (Arr):</font></td><td style="background: #FFFF00;">&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
     </table></div>
     <br>
     <font size="2" color="grey">
