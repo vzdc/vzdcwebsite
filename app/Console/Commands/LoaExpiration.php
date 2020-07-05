dcaa1d3139864a13d99fda8c676d2336ef802c71
Jacob Boyles <fixterjake@gmail.com>
2020-07-05 15:02:02 -0400

    rewrote schedule, hopefully fixes

--- app/Console/Commands/LoaExpiration.php
+++ app/Console/Commands/LoaExpiration.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Loa;
 use App\User;
 use Mail;
-use DateTime;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 
@@ -42,25 +42,22 @@ class LoaExpiration extends Command
      */
     public function handle()
     {
-        $loas = Loa::where('status', 1)->get();
-        $date = new DateTime('now');
+        $today = Carbon::now()->format('m/d/Y');
+        $loas = Loa::where('end_date', $today)->get();
 
         foreach($loas as $loa) {
-            $loa_date = new DateTime($loa->end_date);
-            if ($date <= $loa_date) {
-                $loa->status = 3;
-                $loa->save();
+            $loa->status = 3;
+            $loa->save();
 
-                $user = User::find($loa->controller_id);
-                $user->status = 1;
-                $user->save();
+            $user = User::find($loa->controller_id);
+            $user->status = 1;
+            $user->save();
 
-                Mail::send(['html' => 'emails.loas.expiration'], ['loa' => $loa, 'user' => $user], function ($m) use ($loa) {
-                    $m->from('notams@vzdc.org', 'vZDC LOA Center');
-                    $m->subject('Your vZDC LOA Has Expired');
-                    $m->to($loa->controller_email);
-                });
-            }
+            Mail::send(['html' => 'emails.loas.expiration'], ['loa' => $loa, 'user' => $user], function ($m) use ($loa) {
+                $m->from('notams@vzdc.org', 'vZDC LOA Center');
+                $m->subject('Your vZDC LOA Has Expired');
+                $m->to($loa->controller_email);
+            });
         }
     }
 }
