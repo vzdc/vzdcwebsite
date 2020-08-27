8aaf87273a78e74898093ce110052cf2f54c9992
Jacob Boyles <fixterjake@gmail.com>
2020-08-27 05:21:24 -0400

    Add dossier entry on visitor accept or manual add

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -535,6 +535,12 @@ class AdminDash extends Controller
         $visitor->status = 1;
         $visitor->save();
 
+        $dossier = new MemberLog();
+        $dossier->user_target = $visitor->id;
+        $dossier->user_submitter = Auth::id();
+        $dossier->content = "Visitor added on ". Carbon::now()->format('m/d/Y') . ".";
+        $dossier->save();
+
         Mail::send('emails.visit.accept', ['visitor' => $visitor], function ($message) use ($visitor) {
             $message->from('notams@vzdc.org', 'vZDC Visiting Department')->subject('Visitor Request Accepted');
             $message->to($visitor->email)->cc('datm@vzdc.org')->cc('atm@vzdc.org')->cc('ta@vzdc.org');
@@ -685,6 +691,12 @@ class AdminDash extends Controller
         $validator = $request->validate([
             'cid' => 'required'
         ]);
+
+        $dossier = new MemberLog();
+        $dossier->user_target = $request->cid;
+        $dossier->user_submitter = Auth::id();
+        $dossier->content = "Visitor added on ". Carbon::now()->format('m/d/Y') . ".";
+        $dossier->save();
 
         $client = new Client(['exceptions' => false]);
         $response = $client->request('GET', 'https://api.vatusa.net/v2/user/' . $request->cid . '?apikey=' . Config::get('vatusa.api_key'));
