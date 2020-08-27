3b9aba7d501e7070b73837c099f763befb7eeede
Jacob Boyles <fixterjake@gmail.com>
2020-08-27 04:49:07 -0400

    Date picker data target

--- resources/views/dashboard/controllers/loa.blade.php
+++ resources/views/dashboard/controllers/loa.blade.php
@@ -21,14 +21,14 @@ LOA Request
         {!! Form::label('start_date', 'Starting Date of LOA') !!}
         <div class="input-group date" id="datetimepickerend" data-target-input="nearest">
             {!! Form::text('start_date', null, ['placeholder' => 'MM/DD/YYYY', 'class' => 'form-control datetimepicker-input', 'data-target' => '#datetimepickerend']) !!}
-            <div class="input-group-append" data-target="#datetimepickerstart" data-toggle="datetimepickerstart">
+            <div class="input-group-append" data-target="#datetimepickerstart" data-toggle="datetimepicker">
                 <div class="input-group-text"><i class="fa fa-calendar"></i></div>
             </div>
         </div>
         {!! Form::label('end_date', 'Ending Date of LOA') !!}
         <div class="input-group date" id="datetimepickerend" data-target-input="nearest">
             {!! Form::text('end_date', null, ['placeholder' => 'MM/DD/YYYY', 'class' => 'form-control datetimepicker-input', 'data-target' => '#datetimepickerend']) !!}
-            <div class="input-group-append" data-target="#datetimepickerend" data-toggle="datetimepickerend">
+            <div class="input-group-append" data-target="#datetimepickerend" data-toggle="datetimepicker">
                 <div class="input-group-text"><i class="fa fa-calendar"></i></div>
             </div>
         </div>
