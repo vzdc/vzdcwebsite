d45688801c00173720f2de6d178bec32db171925
Jacob Boyles <fixterjake@gmail.com>
2020-09-02 18:55:42 -0400

    Make header for loa info email bold

--- resources/views/emails/loas/info.blade.php
+++ resources/views/emails/loas/info.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.email')
 
 @section('content')
-    <p>More information is required for your LOA request:</p>
+    <p><b>More information is required for your LOA request</b></p>
     <p>{{ $info }}</p>
     <br>
     <p>Please email datm@vzdc.org with the required information.</p>
