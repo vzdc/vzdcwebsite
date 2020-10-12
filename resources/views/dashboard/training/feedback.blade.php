0ee96b1ba431c775fc8156927919a0679c37463d
Jacob Boyles <fixterjake@jacobs-mbp.myfiosgateway.com>
2020-10-12 04:53:57 -0400

    Forgot button and changed formatting

--- resources/views/dashboard/training/feedback.blade.php
+++ resources/views/dashboard/training/feedback.blade.php
@@ -16,13 +16,20 @@ Feedback
 <div class="container">
     <div class="row">
         <form method="get" href="/dashboard/training/feedback/view">
-            <div class="form-group">
-                <label for="id">Select Controller</label>
-                <select class="form-control" id="id">
-                    @foreach($controller in $controllers)
-                        <option value="{{ $controller->id }}">{{ $controller->backwards_name_with_cid }}</option>
-                    @endforeach
-                </select>
+            <div class="row">
+                <div class="col-sm">
+                    <div class="form-group">
+                        <label for="id">Select Controller</label>
+                        <select class="form-control" id="id">
+                            @foreach($controller in $controllers)
+                                <option value="{{ $controller->id }}">{{ $controller->backwards_name_with_cid }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-1">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </div>
         </form>
     </div>
