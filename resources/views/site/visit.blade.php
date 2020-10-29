cdd2666e63a130f37837960330fc001e9a773736
Aaron <aaron.albertson@gmail.com>
2020-10-29 01:19:06 -0400

    Added Criteria section to visiting page

--- resources/views/site/visit.blade.php
+++ resources/views/site/visit.blade.php
@@ -13,7 +13,14 @@ Visit
     </div>
 </span>
 <br>
-
+<br>
+<h3>Visiting Criteria:<h3>
+-You must currently be a member of a facility.
+-You must currently hold a rating greater than OBS.
+-You must have obtained 50 hours on your current rating.
+-You must have been a member of your current facility for 90 days.
+-You must have held your current rating for 90 days.
+<br>
 <div class="container">
     @if($visitors->value == 1)
     {!! Form::open(['action' => 'FrontController@storeVisit']) !!}
