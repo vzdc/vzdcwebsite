187fa06e551116c672616282ce2a8c7a4b009478
Jacob Boyles <fixterjake@gmail.com>
2020-12-10 17:58:30 -0500

    Add debug info

--- app/Http/Controllers/TrainingDash.php
+++ app/Http/Controllers/TrainingDash.php
@@ -158,21 +158,31 @@ class TrainingDash extends Controller
         $audit->what = Auth::user()->full_name . ' added a training ticket for ' . User::find($ticket->controller_id)->full_name . '.';
         $audit->save();
 
-        $client = new \GuzzleHttp\Client();
-        $client->post(
-            "https://api.vatusa.net/v2/user/" . $ticket->controller_id . "/training/record?apikey=" . Config::get('vatusa.api_key'),
-            array(
-                'form_params' => array(
-                    'instructor_id ' => $ticket->instructor_id,
-                    'session_date' => $ticket->date,
-                    'position' => $ticket->position_central,
-                    'duration' => $ticket->duration,
-                    'notes' => $ticket->comments,
-                    'location' => $ticket->type_central,
-                    'score' => $ticket->score
+        try {
+            $client = new \GuzzleHttp\Client();
+            $client->post(
+                "https://api.vatusa.net/v2/user/" . $ticket->controller_id . "/training/record?apikey=" . Config::get('vatusa.api_key'),
+                array(
+                    'form_params' => array(
+                        'instructor_id ' => $ticket->instructor_id,
+                        'session_date' => $ticket->date,
+                        'position' => $ticket->position_central,
+                        'duration' => $ticket->duration,
+                        'notes' => $ticket->comments,
+                        'location' => $ticket->type_central,
+                        'score' => $ticket->score
+                    )
                 )
-            )
-        );
+            );
+        }
+        catch (Exception $e) {
+            return redirect('/dashboard')->with(
+                'error', 'Instructor ID: ' . $ticket->instructor_id . ' Date: ' . 
+                $ticket->date . ' Position: ' . $ticket->position_central . 'Duration: ' .
+                $ticket->duration . ' Notes: ' . $ticket->comments . ' Location: ' . $ticket->type_central . ' Score: ' . $ticket->score
+            );
+        }
+
         return redirect('/dashboard/training/tickets?id=' . $ticket->controller_id)->with('success', 'The training ticket has been submitted successfully' . $extra . '.');
     }
 
