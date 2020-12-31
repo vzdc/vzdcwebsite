d11890c56ee8df8c553bcf16e8f75d80b04fc21b
Jacob Boyles <fixterjake@gmail.com>
2020-12-31 17:06:43 -0500

    fix shit 75

--- app/Http/Controllers/TrainingDash.php
+++ app/Http/Controllers/TrainingDash.php
@@ -162,6 +162,7 @@ class TrainingDash extends Controller
         $client = new \GuzzleHttp\Client();
 
         try {
+            dd("fuck");
             $res = $client->post("https://api.vatusa.net/v2/user/" . $ticket->controller_id . "/training/record?apikey=" . Config::get('vatusa.api_key'), [
                 'instructor_id ' => $ticket->trainer_id,
                 'session_date' => $date->format('Y-m-d') . " " . $request->start,
