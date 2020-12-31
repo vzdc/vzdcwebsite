bd18f3db70255448a04920b649355accf30f23f2
Jacob Boyles <fixterjake@gmail.com>
2020-12-31 00:38:36 -0500

    fix shit 11

--- app/Http/Controllers/TrainingDash.php
+++ app/Http/Controllers/TrainingDash.php
@@ -169,7 +169,7 @@ class TrainingDash extends Controller
 
         return redirect('/dashboard')->with(
             'error', 'Instructor ID: ' . $ticket->trainer_id . ' Date: ' . 
-            $ticket->date . " " . $request->start . ' Position: ' . $ticket->position_central . 'Duration: ' .
+            $ticket->date->format('Y/m/d') . " " . $request->start . ' Position: ' . $ticket->position_central . 'Duration: ' .
             $ticket->duration . ' Notes: ' . $ticket->comments . ' Location: ' . $ticket->type_central
         );
 
