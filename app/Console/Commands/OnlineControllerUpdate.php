97cd52f3258f6e556668236a8d1738801a3b22a3
Jacob Boyles <fixterjake@gmail.com>
2021-03-01 16:07:21 -0500

    Add JYO

--- app/Console/Commands/OnlineControllerUpdate.php
+++ app/Console/Commands/OnlineControllerUpdate.php
@@ -41,7 +41,7 @@ class OnlineControllerUpdate extends Command
         'RIC_', 'ROA_', 'ORF_', 'ACY_', 'NGU_', 'NTU_', 'NHK_', 'RDU_',
         /* DELTA */
         'CHO_', 'HGR_', 'LYH_', 'EWN_', 'LWB_', 'ISO_', 'MTN_', 'HEF_', 'MRB_', 'PHF_', 'SBY_', 'NUI_',
-        'FAY_', 'ILM_', 'NKT_', 'NCA_', 'NYG_', 'DAA_', 'DOV_', 'POB_', 'GSB_', 'WAL_', 'CVN_',
+        'FAY_', 'ILM_', 'NKT_', 'NCA_', 'NYG_', 'DAA_', 'DOV_', 'POB_', 'GSB_', 'WAL_', 'CVN_', 'JYO_'
     ];
 
     /**
