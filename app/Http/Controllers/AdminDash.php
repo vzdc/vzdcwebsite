ac65beaf6af6bd4aa0c024f124905d9a0690bf09
Jacob Boyles <fixterjake@gmail.com>
2021-03-30 14:49:29 -0400

    Fix redirect after submission

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -1876,7 +1876,7 @@ class AdminDash extends Controller
                 $m->subject('New Manual Dossier Log Entry');
                 $m->to('srstaff@vzdc.org');
             });
-            return View('dashboard.controllers.roster')->with('success', 'The log has been created successfully.');
+            return redirect('/dashboard/controllers/roster')->with('success', 'The log has been created successfully.');
         } else {
             return redirect()->back()->with('error', 'Access Denied.');
         }
