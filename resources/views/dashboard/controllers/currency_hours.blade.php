d3bc7c30bafbc9552d7f746309448f5484ed2827
Aaron <aaron.albertson@gmail.com>
2022-03-23 02:04:47 -0400

    Force update
    Force Update

--- resources/views/dashboard/controllers/currency_hours.blade.php
+++ resources/views/dashboard/controllers/currency_hours.blade.php
@@ -8,3 +8,4 @@
     <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSsaWm6W07o2lexUAmCnbamYXID48ZtHLoZVOwHExO06GW8JRPnVtbYPlFfk-LfNg8IJ1ueaKaUDi1P/pubhtml?widget=true&amp;headers=true"
             width="100%" height="800"></iframe>
 @endsection
+1
